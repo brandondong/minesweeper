d8b15ce98aa79784a149e1e97554e20c028b667d
Brandon <brandondong604@hotmail.com>
2020-07-14 19:37:29 -0700

    Add tiles

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,14 @@ struct Model {
 }
 
 enum Difficulty {
-    Easy,
-    Medium,
-    Hard,
+    Easy([i32; 10 * 8]),
+    Medium([i32; 18 * 14]),
+    Hard([i32; 24 * 20]),
 }
 
 impl Default for Difficulty {
     fn default() -> Self {
-        Self::Medium
+        Self::Medium([0; 18 * 14])
     }
 }
 
@@ -44,50 +44,59 @@ fn view(model: &Model) -> Node<Msg> {
 
 fn view_game(model: &Model) -> Node<Msg> {
     let (width_class, ratio_class, tile_class) = match model.difficulty {
-        Difficulty::Easy => ("Game_easyWidth", "Game_easyHeightRatio", "Game_easyTile"),
-        Difficulty::Medium => (
+        Difficulty::Easy(_) => ("Game_easyWidth", "Game_easyHeightRatio", "Game_easyTile"),
+        Difficulty::Medium(_) => (
             "Game_mediumWidth",
             "Game_mediumHeightRatio",
             "Game_mediumTile",
         ),
-        Difficulty::Hard => ("Game_hardWidth", "Game_hardHeightRatio", "Game_hardTile"),
+        Difficulty::Hard(_) => ("Game_hardWidth", "Game_hardHeightRatio", "Game_hardTile"),
     };
+    let tiles = get_board(model)
+        .iter()
+        .map(|i| div![C![tile_class], view_tile(i)]);
     div![
         C![width_class],
         view_header(model),
-        div![
-            C![ratio_class],
-            div![C!["Game_boardContainer"], div![C![tile_class], "A"],]
-        ]
+        div![C![ratio_class], div![C!["Game_boardContainer"], tiles,]]
     ]
 }
 
 fn view_header(model: &Model) -> Node<Msg> {
+    let easy_selected = matches!(model.difficulty, Difficulty::Easy(_));
+    let medium_selected = matches!(model.difficulty, Difficulty::Medium(_));
+    let hard_selected = matches!(model.difficulty, Difficulty::Hard(_));
     select![
         el_ref(&model.select_difficulty_element),
         ev(Ev::Change, |_| {
             Msg::DifficultyChange(header_select_mapping)
         }),
+        option![attrs! {At::Selected => easy_selected.as_at_value()}, "Easy"],
         option![
-            attrs! {At::Selected => matches!(model.difficulty, Difficulty::Easy).as_at_value()},
-            "Easy"
-        ],
-        option![
-            attrs! {At::Selected => matches!(model.difficulty, Difficulty::Medium).as_at_value()},
+            attrs! {At::Selected => medium_selected.as_at_value()},
             "Medium"
         ],
-        option![
-            attrs! {At::Selected => matches!(model.difficulty, Difficulty::Hard).as_at_value()},
-            "Hard"
-        ]
+        option![attrs! {At::Selected => hard_selected.as_at_value()}, "Hard"]
     ]
+}
+
+fn view_tile(model: &i32) -> Node<Msg> {
+    div![C!["Tile_container"], model]
 }
 
 fn header_select_mapping(index: i32) -> Difficulty {
     match index {
-        0 => Difficulty::Easy,
-        1 => Difficulty::Medium,
-        _ => Difficulty::Hard,
+        0 => Difficulty::Easy([0; 10 * 8]),
+        1 => Difficulty::Medium([0; 18 * 14]),
+        _ => Difficulty::Hard([0; 24 * 20]),
+    }
+}
+
+fn get_board(model: &Model) -> &[i32] {
+    match &model.difficulty {
+        Difficulty::Easy(a) => a,
+        Difficulty::Medium(a) => a,
+        Difficulty::Hard(a) => a,
     }
 }
 
