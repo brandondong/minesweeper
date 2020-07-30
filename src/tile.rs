2ef9df84ec8f561eb3e11f13f80f6f2abe6d143a
Brandon <brandondong604@hotmail.com>
2020-07-29 21:41:45 -0700

    Start work on tile styling

--- src/tile.rs
+++ src/tile.rs
@@ -1,12 +1,15 @@
+use crate::icons::view_flag_icon;
 use crate::{HiddenState, Msg, Tile, VisibleState};
 use seed::{prelude::*, *};
 
 pub(crate) fn view_tile(tile: &Tile) -> Node<Msg> {
-    let s = match (tile.visible_state, tile.hidden_state) {
-        (VisibleState::Covered, _) => "c",
-        (VisibleState::Flagged, _) => "f",
-        (VisibleState::Uncovered, HiddenState::Safe) => "s",
-        (VisibleState::Uncovered, HiddenState::Mine) => "m",
-    };
-    div![C!["Tile_container"], s]
+    match (tile.visible_state, tile.hidden_state) {
+        (VisibleState::Covered, _) => div![C!["Tile_coveredBackground"], "c"],
+        (VisibleState::Flagged, _) => div![
+            C!["Tile_coveredBackground"],
+            view_flag_icon(C!["Tile_flagIcon"])
+        ],
+        (VisibleState::Uncovered, HiddenState::Safe) => div![C!["Tile_uncoveredBackground"], "s"],
+        (VisibleState::Uncovered, HiddenState::Mine) => div![C!["Tile_uncoveredBackground"], "m"],
+    }
 }
