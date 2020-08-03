139be6b3dfc7a6e187e7dd518460b97c2ec86149
Brandon <brandondong604@hotmail.com>
2020-08-03 10:51:03 -0700

    Add transition on tile click

--- src/tile.rs
+++ src/tile.rs
@@ -3,16 +3,26 @@ use crate::{HiddenState, Msg, Tile, VisibleState};
 use seed::{prelude::*, *};
 
 pub(crate) fn view_tile(tile: &Tile) -> Node<Msg> {
-    match (tile.visible_state, tile.hidden_state) {
-        (VisibleState::Covered, _) => div![
+    let top_layer = match tile.visible_state {
+        VisibleState::Covered => div![
             C!["Tile_coveredBackground"],
             view_flag_icon(C!["Tile_flagIcon", "Tile_flagIconHidden"])
         ],
-        (VisibleState::Flagged, _) => div![
+        VisibleState::Flagged => div![
             C!["Tile_coveredBackground"],
             view_flag_icon(C!["Tile_flagIcon"])
         ],
-        (VisibleState::Uncovered, HiddenState::Safe) => div![C!["Tile_uncoveredBackground"], "s"],
-        (VisibleState::Uncovered, HiddenState::Mine) => div![C!["Tile_uncoveredBackground"], "m"],
-    }
+        VisibleState::Uncovered => {
+            div![C!["Tile_coveredBackground", "Tile_coveredBackgroundHidden"]]
+        }
+    };
+    let bottom_layer = match tile.hidden_state {
+        HiddenState::Safe => "s",
+        HiddenState::Mine => "m",
+    };
+    div![
+        C!["Tile_container"],
+        div![C!["Tile_topLayer"], top_layer],
+        bottom_layer
+    ]
 }
